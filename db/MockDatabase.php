9a8c9088e95d165bf896e23176579328027d9327
carloeusebi <carloeusebi@gmail.it>
2023-07-24 16:10:39 +0200

    Crated Product Instances

--- db/MockDatabase.php
+++ db/MockDatabase.php
@@ -10,6 +10,7 @@ class MockDatabase
                 "product_name" => "Royal Canin Mini Adult",
                 "animal_type" => "dog",
                 "price" => 43.99,
+                "img_url" => '',
                 "weight_in_grams" => 545,
                 "ingredients" => ["ham", "rise"]
             ],
@@ -18,6 +19,7 @@ class MockDatabase
                 "product_name" => "Almo Nature Holistic Maintenance Medium Large Tonno e Riso",
                 "animal_type" => "dog",
                 "price" => 44.99,
+                "img_url" => '',
                 "weight_in_grams" => 600,
                 "ingredients" => ["beef", "cereals"]
             ],
@@ -26,6 +28,7 @@ class MockDatabase
                 "product_name" => "Almo Nature Cat Daily Lattina",
                 "animal_type" => "cat",
                 "price" => 34.99,
+                "img_url" => '',
                 "weight_in_grams" => 400,
                 "ingredients" => ["tuna", "chicken", "ham",]
             ],
@@ -33,6 +36,7 @@ class MockDatabase
                 "id" => 4,
                 "product_name" => "Mangime per Pesci Gruppy in Fiocchi",
                 "animal_type" => "fish",
+                "img_url" => '',
                 "price" => 2.95,
                 "weight_in_grams" => 30,
                 "ingredients" => ["fish products", "cereals", "algas", "yeast"]
@@ -49,6 +53,7 @@ class MockDatabase
                 "product_name" => "Voliera Wilma in Legno",
                 "animal_type" => "bird",
                 "price" => 184.99,
+                "img_url" => '',
                 "material" => "wood",
                 "dimensions" => "M: L 83 x P 67 x H 153cm"
             ],
@@ -57,6 +62,7 @@ class MockDatabase
                 "product_name" => "Cartucce Filtranti per Filtro EasyCrystal",
                 "animal_type" => "fish",
                 "price" => 2.29,
+                "img_url" => '',
                 "material" => "foam material",
                 // no dimensions :)
             ]
@@ -71,6 +77,7 @@ class MockDatabase
                 "product_name" => "Kong Classic",
                 "animal_type" => "dog",
                 "price" => 13.49,
+                "img_url" => '',
                 "features" => "floats and bounces",
                 "dimensions" => "8,5cm x 10cm"
             ],
@@ -79,6 +86,7 @@ class MockDatabase
                 "product_name" => "Topini di peluche Trixie",
                 "animal_type" => "cat",
                 "price" => 4.99,
+                "img_url" => '',
                 "features" => "soft with tail in rope",
                 "dimensions" => "8,5cm x 10cm"
             ]
