af35095d60f9ff099dddf4f98bdddfa27bebcabe
carloeusebi <carloeusebi@gmail.it>
2023-07-24 16:14:30 +0200

    Moved main logic outside index.php

---
+++ controllers/products_controller.php
@@ -0,0 +1,40 @@
+<?php
+require_once __DIR__ . '/../db/MockDatabase.php';
+
+require_once __DIR__ . '/../models/BaseProduct.php';
+require_once __DIR__ . '/../models/AccessoriesProduct.php';
+require_once __DIR__ . '/../models/FoodProduct.php';
+require_once __DIR__ . '/../models/ToyProduct.php';
+
+
+$fetched_accessories = MockDatabase::getAccessoriesProducts();
+$fetched_foods = MockDatabase::getFoodProducts();
+$fetched_toys = MockDatabase::getToyProducts();
+
+/**
+ * @var BaseProduct[]
+ */
+$products = [];
+
+// instancing food products
+foreach ($fetched_foods as $food) {
+    extract($food);
+    $food_model = new FoodProduct($id, $product_name, $animal_type, $price, $img_url, $weight_in_grams, $ingredients);
+    $products[] = $food_model;
+}
+
+// instancing accessory products
+foreach ($fetched_accessories as $accessory) {
+    extract($accessory);
+    $accessory_model = new AccessoriesProduct($id, $product_name, $animal_type, $price, $img_url, $material, $dimensions);
+    $products[] = $accessory_model;
+}
+
+// instancing toys products
+foreach ($fetched_toys as $toy) {
+    extract($toy);
+    $toy_model = new ToyProduct($id, $product_name, $animal_type, $price, $img_url, $features, $dimensions);
+    $products[] = $toy_model;
+}
+
+return $products;
--- index.php
+++ index.php
@@ -9,33 +9,9 @@ require_once __DIR__ . '/models/AccessoriesProduct.php';
 require_once __DIR__ . '/models/FoodProduct.php';
 require_once __DIR__ . '/models/ToyProduct.php';
 
-
-$fetched_accessories = MockDatabase::getAccessoriesProducts();
-$fetched_foods = MockDatabase::getFoodProducts();
-$fetched_toys = MockDatabase::getToyProducts();
-
 /**
  * @var BaseProduct[]
  */
-$products = [];
-
-foreach ($fetched_foods as $food) {
-    extract($food);
-    $food_model = new FoodProduct($id, $product_name, $animal_type, $price, $img_url, $weight_in_grams, $ingredients);
-    $products[] = $food_model;
-}
-
-foreach ($fetched_accessories as $accessory) {
-    extract($accessory);
-    $accessory_model = new AccessoriesProduct($id, $product_name, $animal_type, $price, $img_url, $material, $dimensions);
-    $products[] = $accessory_model;
-}
-
-foreach ($fetched_toys as $toy) {
-    extract($toy);
-    $toy_model = new ToyProduct($id, $product_name, $animal_type, $price, $img_url, $features, $dimensions);
-    $products[] = $toy_model;
-}
-
+$products = require_once __DIR__ . '/controllers/products_controller.php';
 
 dd($products);
