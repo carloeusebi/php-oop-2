d735d444b9588f4916472be5326645696cf474dd
carloeusebi <carloeusebi@gmail.it>
2023-07-24 17:11:57 +0200

    Added all images url

--- db/MockDatabase.php
+++ db/MockDatabase.php
@@ -28,7 +28,7 @@ class MockDatabase
                 "product_name" => "Almo Nature Cat Daily Lattina",
                 "animal_type" => "cat",
                 "price" => 34.99,
-                "img_url" => '',
+                "img_url" => '245336/almo-daily-menu-cat-400-gr-vitello.jpg',
                 "weight_in_grams" => 400,
                 "ingredients" => ["tuna", "chicken", "ham",]
             ],
@@ -36,7 +36,7 @@ class MockDatabase
                 "id" => 4,
                 "product_name" => "Mangime per Pesci Gruppy in Fiocchi",
                 "animal_type" => "fish",
-                "img_url" => '',
+                "img_url" => '272714/tetra-guppy-mini-flakes.jpg',
                 "price" => 2.95,
                 "weight_in_grams" => 30,
                 "ingredients" => ["fish products", "cereals", "algas", "yeast"]
@@ -53,7 +53,7 @@ class MockDatabase
                 "product_name" => "Voliera Wilma in Legno",
                 "animal_type" => "bird",
                 "price" => 184.99,
-                "img_url" => '',
+                "img_url" => '258384/voliera-wilma1.jpg',
                 "material" => "wood",
                 "dimensions" => "M: L 83 x P 67 x H 153cm"
             ],
@@ -62,7 +62,7 @@ class MockDatabase
                 "product_name" => "Cartucce Filtranti per Filtro EasyCrystal",
                 "animal_type" => "fish",
                 "price" => 2.29,
-                "img_url" => '',
+                "img_url" => '272741/tetra-easycrystal-filterpack-250-300.jpg',
                 "material" => "foam material",
                 // no dimensions :)
             ]
@@ -77,7 +77,7 @@ class MockDatabase
                 "product_name" => "Kong Classic",
                 "animal_type" => "dog",
                 "price" => 13.49,
-                "img_url" => '',
+                "img_url" => '256599/kong-classic1.jpg',
                 "features" => "floats and bounces",
                 "dimensions" => "8,5cm x 10cm"
             ],
@@ -86,7 +86,7 @@ class MockDatabase
                 "product_name" => "Topini di peluche Trixie",
                 "animal_type" => "cat",
                 "price" => 4.99,
-                "img_url" => '',
+                "img_url" => '223852/trixie-gatto-gioco-active-mouse-peluche.jpg',
                 "features" => "soft with tail in rope",
                 "dimensions" => "8,5cm x 10cm"
             ]
